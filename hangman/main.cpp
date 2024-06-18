9bc28181be4848b0c159ab6a4478f0bd3eb159e4
Finlay Turner <finlayturner109@gmail.com>
2024-06-18 21:02:03 +0100

    Project updated

--- hangman/main.cpp
+++ hangman/main.cpp
@@ -9,44 +9,29 @@
 #include <string>
 #include <cctype>
 
+bool game( int *score);
 bool checkans(std::string &guess, std::string &answer);
-bool game();
+
 
 int main(int argc, const char * argv[]) {
+    int score = 0;
     
-    while(game());
+    while(game(&score));
     
-    std::cout << std::endl << "Thanks for playing!" << std::endl;
+    std::cout << std::endl << "Thanks for playing!" << std::endl << "You won " << score;
+    if (score == 1)
+    {
+        std::cout << " game" << std::endl;
+    }
+    else
+    {
+        std::cout << " games" << std::endl;
+    }
     
     return 0;
 }
 
-bool checkans(std::string &guess, std::string &answer)
-{
-    std::string guessLower;
-    std::string answerLower;
-    
-    // Convert guess into lowercase
-    for (int i = 0; i < guess.length(); i++)
-    {
-        guessLower += std::tolower(guess[i]);
-    }
-    
-    // Convert answer into lowercase
-    for (int i = 0; i < answer.length(); i++)
-    {
-        answerLower += static_cast<char>(std::tolower(answer[i]));
-    }
-    
-    // Compare answer to guess
-    if (guessLower == answerLower)
-    {
-        return true;
-    }
-    return false;
-}
-
-bool game()
+bool game(int *score)
 {
     std::string ans = "Steve Jobs";
     std::string wordProgress;
@@ -116,6 +101,7 @@ bool game()
     if (userHasWon)
     {
         std::string response;
+        *score = *score + 1;
         std::cout << std::endl << "Correct! The answer was " << ans << std::endl <<  "Would you like to play again? (y/n)";
         std::getline(std::cin, response);
         
@@ -128,5 +114,45 @@ bool game()
             return false;
         }
     }
-    return 1;
+    
+    else
+    {
+        std::string response;
+        std::cout << std::endl << "You lose!" << std::endl <<  "Would you like to play again? (y/n)";
+        std::getline(std::cin, response);
+        
+        if (response[0] == 'y' or response[0] == 'Y')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+bool checkans(std::string &guess, std::string &answer)
+{
+    std::string guessLower;
+    std::string answerLower;
+    
+    // Convert guess into lowercase
+    for (int i = 0; i < guess.length(); i++)
+    {
+        guessLower += std::tolower(guess[i]);
+    }
+    
+    // Convert answer into lowercase
+    for (int i = 0; i < answer.length(); i++)
+    {
+        answerLower += static_cast<char>(std::tolower(answer[i]));
+    }
+    
+    // Compare answer to guess
+    if (guessLower == answerLower)
+    {
+        return true;
+    }
+    return false;
 }
